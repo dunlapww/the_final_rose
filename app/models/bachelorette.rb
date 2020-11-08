c9b02c73618a882d7939941da66987371ac6ccab
Will Dunlap <dunlapww@gmail.com>
2020-11-08 14:57:45 -0700

    update season and bachelorette models to reflect new relationship

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -1,4 +1,4 @@
 class Bachelorette <ApplicationRecord
   has_many :contestants
-  
+  belongs_to :season  
 end
---
+++ spec/features/bachelorettes/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe "When i visit the bachelorette's show page ('/bachelorettes/:id')" do
+    before :each do
+      @season = Season.create!({description: "Young lady find a hunk"})
+      @sara = @season.bachelorettes.create!({name: "Sara"})
+      @jeff = @sara.contestants.create!({name: "Jeff", age: 25, hometown: "Boulder"})
+      @steve = @sara.contestants.create!({name: "Steve", age: 30, hometown: "New York City"})
+      @eric = @sara.contestants.create!({name: "Eric", age: 28, hometown: "Madison"})
+    end
+    it "I see the bachelorettes Name, season number, description of season" do
+      visit "/bachelorettes/#{@sara.id}"
+      within("#b-details") do
+        expect(page).to have_content(@sara.name)
+        expect(page).to have_content(@sara.season.id)
+        expect(page).to have_content(@sara.season.description)
+      end
+    end
+    it "i see a link to see the bachelorette's contestants ('/bachelorettes/:bachelorette_id/contestants/')" do
+      visit "/bachelorettes/#{@sara.id}"
+      click_on "contestants"
+      expect(current_path).to eq("/bachelorettes/#{@sara.id}/contestants")
+    end
+    it 'on the contestants index page I see only that bachelorettes contestants'
+  end
+end
