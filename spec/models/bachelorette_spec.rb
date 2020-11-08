ad18d59a1ba6fc17bf6067b2448e94625bf2312b
Will Dunlap <dunlapww@gmail.com>
2020-11-08 16:38:32 -0700

    Add model test for avg_contestant_age

--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Bachelorette, type: :model do
     it {should have_many :contestants}
     it {should belong_to :season}
   end
+
+  describe 'instance methods' do
+    it '#avg_contestant_age' do
+      @season = Season.create!({description: "Young lady find a hunk"})
+      @sara = @season.bachelorettes.create!({name: "Sara"})
+      @jeff = @sara.contestants.create!({name: "Jeff", age: 25, hometown: "Boulder"})
+      @steve = @sara.contestants.create!({name: "Steve", age: 30, hometown: "New York City"})
+      @eric = @sara.contestants.create!({name: "Eric", age: 28, hometown: "Madison"})
+  
+      expect(@sara.avg_contestant_age.round(0)).to eq(28)
+    end
+  end
 end
